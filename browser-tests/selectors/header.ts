33eff8df3cca06883c3a0e6e7f7626523fe381fd
Jori Lindell <jori.lindell@gmail.com>
2020-09-22 17:37:48 +0300

    Add TestCafe and first simple test

---
+++ browser-tests/selectors/header.ts
@@ -0,0 +1,7 @@
+import { Selector } from 'testcafe';
+
+export const header = {
+  languageSelector: Selector('#languageDropdown-toggle-button'),
+  languageSelectorItem0: Selector('#languageDropdown-item-0'),
+  languageSelectorItem1: Selector('#languageDropdown-item-0'),
+};
