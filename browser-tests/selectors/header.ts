121657f2a108e213a2263f4e34ecaf4a5c2fed57
Jori Lindell <jori.lindell@gmail.com>
2020-09-22 17:39:48 +0300

    Fix small typo

--- browser-tests/selectors/header.ts
+++ browser-tests/selectors/header.ts
@@ -3,5 +3,5 @@ import { Selector } from 'testcafe';
 export const header = {
   languageSelector: Selector('#languageDropdown-toggle-button'),
   languageSelectorItem0: Selector('#languageDropdown-item-0'),
-  languageSelectorItem1: Selector('#languageDropdown-item-0'),
+  languageSelectorItem1: Selector('#languageDropdown-item-1'),
 };
