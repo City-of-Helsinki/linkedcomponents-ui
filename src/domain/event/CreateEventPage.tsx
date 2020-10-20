5ff3661ad02d9491130bbf33c1e59f0f33c54cc4
Jori Lindell <jori.lindell@gmail.com>
2020-10-20 12:25:23 +0300

    Type section of the event form

--- src/domain/event/CreateEventPage.tsx
+++ src/domain/event/CreateEventPage.tsx
@@ -1,76 +1,113 @@
+import { Formik } from 'formik';
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 
+import Button from '../../common/components/button/Button';
+import Container from '../app/layout/Container';
 import PageWrapper from '../app/layout/PageWrapper';
+import { EVENT_INITIALVALUES } from './constants';
 import EventNavigation from './eventNavigation/EventNavigation';
+import styles from './eventPage.module.scss';
+import TypeSection from './formSections/TypeSection';
+import { createValidationSchema } from './utils';
 
 const CreateEventPage: React.FC = () => {
   const { t } = useTranslation();
+  const validationSchema = createValidationSchema();
 
   return (
-    <PageWrapper title="createEventPage.pageTitle">
-      <EventNavigation
-        items={[
-          {
-            component: <div>{t('event.navigation.steps.type')}</div>,
-            isCompleted: true,
-            label: t('event.navigation.steps.type'),
-          },
-          {
-            component: <div>{t('event.navigation.steps.languages')}</div>,
-            isCompleted: false,
-            label: t('event.navigation.steps.languages'),
-          },
-          {
-            component: (
-              <div>{t('event.navigation.steps.responsibilities')}</div>
-            ),
-            isCompleted: false,
-            label: t('event.navigation.steps.responsibilities'),
-          },
-          {
-            component: <div>{t('event.navigation.steps.time')}</div>,
-            isCompleted: false,
-            label: t('event.navigation.steps.time'),
-          },
-          {
-            component: <div>{t('event.navigation.steps.place')}</div>,
-            isCompleted: false,
-            label: t('event.navigation.steps.place'),
-          },
-          {
-            component: <div>{t('event.navigation.steps.price')}</div>,
-            isCompleted: false,
-            label: t('event.navigation.steps.price'),
-          },
-          {
-            component: <div>{t('event.navigation.steps.media')}</div>,
-            isCompleted: false,
-            label: t('event.navigation.steps.media'),
-          },
-          {
-            component: <div>{t('event.navigation.steps.classification')}</div>,
-            isCompleted: false,
-            label: t('event.navigation.steps.classification'),
-          },
-          {
-            component: <div>{t('event.navigation.steps.targets')}</div>,
-            isCompleted: false,
-            label: t('event.navigation.steps.targets'),
-          },
-          {
-            component: <div>{t('event.navigation.steps.commitment')}</div>,
-            isCompleted: false,
-            label: t('event.navigation.steps.commitment'),
-          },
-          {
-            component: <div>{t('event.navigation.steps.ready')}</div>,
-            disabled: true,
-            isCompleted: false,
-            label: t('event.navigation.steps.ready'),
-          },
-        ]}
-      ></EventNavigation>
+    <PageWrapper className={styles.eventPage} title="createEventPage.pageTitle">
+      <Formik
+        initialValues={EVENT_INITIALVALUES}
+        onSubmit={(values) => {
+          console.log('TODO: Submit event form with values: ', values);
+        }}
+        validationSchema={validationSchema}
+      >
+        {() => {
+          return (
+            <>
+              <Container>
+                <div className={styles.titleRow}>
+                  <h1>{t('createEventPage.title')}</h1>
+                  <Button disabled={true} type="submit">
+                    {t('createEventPage.buttonPublish')}
+                  </Button>
+                </div>
+              </Container>
+
+              <EventNavigation
+                items={[
+                  {
+                    component: <TypeSection />,
+                    isCompleted: true,
+                    label: t('event.navigation.steps.type'),
+                  },
+                  {
+                    component: (
+                      <div>{t('event.navigation.steps.languages')}</div>
+                    ),
+                    isCompleted: false,
+                    label: t('event.navigation.steps.languages'),
+                  },
+                  {
+                    component: (
+                      <div>{t('event.navigation.steps.responsibilities')}</div>
+                    ),
+                    isCompleted: false,
+                    label: t('event.navigation.steps.responsibilities'),
+                  },
+                  {
+                    component: <div>{t('event.navigation.steps.time')}</div>,
+                    isCompleted: false,
+                    label: t('event.navigation.steps.time'),
+                  },
+                  {
+                    component: <div>{t('event.navigation.steps.place')}</div>,
+                    isCompleted: false,
+                    label: t('event.navigation.steps.place'),
+                  },
+                  {
+                    component: <div>{t('event.navigation.steps.price')}</div>,
+                    isCompleted: false,
+                    label: t('event.navigation.steps.price'),
+                  },
+                  {
+                    component: <div>{t('event.navigation.steps.media')}</div>,
+                    isCompleted: false,
+                    label: t('event.navigation.steps.media'),
+                  },
+                  {
+                    component: (
+                      <div>{t('event.navigation.steps.classification')}</div>
+                    ),
+                    isCompleted: false,
+                    label: t('event.navigation.steps.classification'),
+                  },
+                  {
+                    component: <div>{t('event.navigation.steps.targets')}</div>,
+                    isCompleted: false,
+                    label: t('event.navigation.steps.targets'),
+                  },
+                  {
+                    component: (
+                      <div>{t('event.navigation.steps.commitment')}</div>
+                    ),
+                    isCompleted: false,
+                    label: t('event.navigation.steps.commitment'),
+                  },
+                  {
+                    component: <div>{t('event.navigation.steps.ready')}</div>,
+                    disabled: true,
+                    isCompleted: false,
+                    label: t('event.navigation.steps.ready'),
+                  },
+                ]}
+              ></EventNavigation>
+            </>
+          );
+        }}
+      </Formik>
     </PageWrapper>
   );
 };
---
+++ src/domain/event/constants.ts
@@ -0,0 +1,8 @@
+export enum EVENT_TYPE {
+  COURSE = 'course',
+  EVENT = 'event',
+}
+
+export const EVENT_INITIALVALUES = {
+  type: EVENT_TYPE.EVENT,
+};
