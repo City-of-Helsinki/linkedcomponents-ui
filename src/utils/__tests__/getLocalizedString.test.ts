a14d98a77ab572a8fbc52d3ef4b5b37ebec207fd
Jori Lindell <jori.lindell@gmail.com>
2020-10-20 20:38:04 +0300

    getLocalisedString helper function

---
+++ src/utils/__tests__/getLocalizedString.test.ts
@@ -0,0 +1,17 @@
+import getLocalisedString from '../getLocalisedString';
+
+const dummyLocalisedObj = {
+  en: 'text en',
+  fi: 'text fi',
+};
+
+describe('getLocalisedString function', () => {
+  it('should return localised string', () => {
+    expect(getLocalisedString(dummyLocalisedObj, 'en')).toBe('text en');
+    expect(getLocalisedString(dummyLocalisedObj, 'fi')).toBe('text fi');
+  });
+
+  it('should return string in backup language value with selected language is not found', () => {
+    expect(getLocalisedString(dummyLocalisedObj, 'sv')).toBe('text fi');
+  });
+});
