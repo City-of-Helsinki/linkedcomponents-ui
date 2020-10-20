946447164b03572484b4fc8a543c5c7bd1cc9866
Jori Lindell <jori.lindell@gmail.com>
2020-10-20 18:14:34 +0300

    LoadingSpinner component

---
+++ src/common/components/loadingSpinner/__tests__/LoadingSpinner.test.tsx
@@ -0,0 +1,22 @@
+import { render } from '@testing-library/react';
+import React from 'react';
+
+import LoadingSpinner from '../LoadingSpinner';
+
+it('render spinner if isLoading is true', () => {
+  const { container } = render(<LoadingSpinner isLoading={true} />);
+  expect((container.firstChild.firstChild as HTMLElement).classList).toContain(
+    'spinner'
+  );
+});
+
+it('render child component if isLoading is false', () => {
+  const { container } = render(
+    <LoadingSpinner isLoading={false}>
+      <div className="component"></div>
+    </LoadingSpinner>
+  );
+  expect((container.firstChild as HTMLElement).classList).toContain(
+    'component'
+  );
+});
