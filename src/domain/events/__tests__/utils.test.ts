a46283e1e46249633d47a244e706771e9b851804
Jori Lindell <jori.lindell@gmail.com>
2020-10-25 18:23:06 +0200

    Tests to eventPathBuilder and eventsPathBuilder functions

---
+++ src/domain/events/__tests__/utils.test.ts
@@ -0,0 +1,115 @@
+import { eventsPathBuilder } from '../utils';
+
+describe('eventsPathBuilder function', () => {
+  it('should create correct path for events request', () => {
+    const items = [
+      {
+        args: { combinedText: ['text1', 'text2'] },
+        expectedPath: '/event/?combined_text=text1,text2',
+      },
+      {
+        args: { division: ['division1', 'division2'] },
+        expectedPath: '/event/?division=division1,division2',
+      },
+      {
+        args: { end: '2020-12-12' },
+        expectedPath: '/event/?end=2020-12-12',
+      },
+      {
+        args: { endsAfter: '14' },
+        expectedPath: '/event/?ends_after=14',
+      },
+      {
+        args: { endsBefore: '14' },
+        expectedPath: '/event/?ends_before=14',
+      },
+      {
+        args: { include: ['include1', 'include2'] },
+        expectedPath: '/event/?include=include1,include2',
+      },
+      {
+        args: { inLanguage: 'fi' },
+        expectedPath: '/event/?in_language=fi',
+      },
+      {
+        args: { isFree: true },
+        expectedPath: '/event/?is_free=true',
+      },
+      {
+        args: { isFree: false },
+        expectedPath: '/event/?is_free=false',
+      },
+      {
+        args: { keyword: ['keyword1', 'keyword2'] },
+        expectedPath: '/event/?keyword=keyword1,keyword2',
+      },
+      {
+        args: { keywordAnd: ['keyword1', 'keyword2'] },
+        expectedPath: '/event/?keyword_AND=keyword1,keyword2',
+      },
+      {
+        args: { keywordNot: ['keyword1', 'keyword2'] },
+        expectedPath: '/event/?keyword!=keyword1,keyword2',
+      },
+      {
+        args: { language: 'fi' },
+        expectedPath: '/event/?language=fi',
+      },
+      {
+        args: { location: ['location1', 'location2'] },
+        expectedPath: '/event/?location=location1,location2',
+      },
+      {
+        args: { publisher: 'hel:123' },
+        expectedPath: '/event/?publisher=hel:123',
+      },
+      {
+        args: { page: 2 },
+        expectedPath: '/event/?page=2',
+      },
+      {
+        args: { pageSize: 10 },
+        expectedPath: '/event/?page_size=10',
+      },
+      {
+        args: { sort: 'start' },
+        expectedPath: '/event/?sort=start',
+      },
+      {
+        args: { start: '2020-12-12' },
+        expectedPath: '/event/?start=2020-12-12',
+      },
+      {
+        args: { startsAfter: '14' },
+        expectedPath: '/event/?starts_after=14',
+      },
+      {
+        args: { startsBefore: '14' },
+        expectedPath: '/event/?starts_before=14',
+      },
+      {
+        args: { superEvent: 'hel:123' },
+        expectedPath: '/event/?super_event=hel:123',
+      },
+      {
+        args: { superEventType: ['type1', 'type2'] },
+        expectedPath: '/event/?super_event_type=type1,type2',
+      },
+      {
+        args: { text: 'text' },
+        expectedPath: '/event/?text=text',
+      },
+      {
+        args: { translation: 'fi' },
+        expectedPath: '/event/?translation=fi',
+      },
+    ];
+
+    items.forEach(({ args, expectedPath }) => {
+      const path = eventsPathBuilder({
+        args,
+      });
+      expect(path).toBe(expectedPath);
+    });
+  });
+});
--- src/domain/events/utils.ts
+++ src/domain/events/utils.ts
@@ -1,5 +1,5 @@
 import { EventsQueryVariables } from '../../generated/graphql';
-import composeQuery from '../../utils/composeQuery';
+import queryBuilder from '../../utils/queryBuilder';
 
 interface EventsPathBuilderProps {
   args: EventsQueryVariables;
@@ -32,81 +32,35 @@ export const eventsPathBuilder = ({ args }: EventsPathBuilderProps) => {
     text,
     translation,
   } = args;
-  // Get details of all needed fields
-  let query = '';
 
-  if (combinedText && combinedText.length) {
-    query = composeQuery(query, 'combined_text', combinedText.join(','));
-  }
-  if (division && division.length) {
-    query = composeQuery(query, 'division', division.join(','));
-  }
-  if (end) {
-    query = composeQuery(query, 'end', end);
-  }
-  if (endsAfter) {
-    query = composeQuery(query, 'ends_after', endsAfter);
-  }
-  if (endsBefore) {
-    query = composeQuery(query, 'ends_before', endsBefore);
-  }
-  if (inLanguage) {
-    query = composeQuery(query, 'in_language', inLanguage);
-  }
-  if (include && include.length) {
-    query = composeQuery(query, 'include', include.join(','));
-  }
-  if (isFree != null) {
-    query = composeQuery(query, 'is_free', isFree ? 'true' : 'false');
-  }
-  if (keyword && keyword.length) {
-    query = composeQuery(query, 'keyword', keyword.join(','));
-  }
-  if (keywordAnd && keywordAnd.length) {
-    query = composeQuery(query, 'keyword_AND', keywordAnd.join(','));
-  }
-  if (keywordNot && keywordNot.length) {
-    query = composeQuery(query, 'keyword!', keywordNot.join(','));
-  }
-  if (language) {
-    query = composeQuery(query, 'language', language);
-  }
-  if (location && location.length) {
-    query = composeQuery(query, 'location', location.join(','));
-  }
-  if (page) {
-    query = composeQuery(query, 'page', page.toString());
-  }
-  if (pageSize) {
-    query = composeQuery(query, 'page_size', pageSize.toString());
-  }
-  if (publisher) {
-    query = composeQuery(query, 'publisher', publisher);
-  }
-  if (sort) {
-    query = composeQuery(query, 'sort', sort);
-  }
-  if (start) {
-    query = composeQuery(query, 'start', start);
-  }
-  if (startsAfter) {
-    query = composeQuery(query, 'starts_after', startsAfter);
-  }
-  if (startsBefore) {
-    query = composeQuery(query, 'starts_before', startsBefore);
-  }
-  if (superEvent) {
-    query = composeQuery(query, 'super_event', superEvent);
-  }
-  if (superEventType && superEventType.length) {
-    query = composeQuery(query, 'super_event_type', superEventType.join(','));
-  }
-  if (text) {
-    query = composeQuery(query, 'text', text);
-  }
-  if (translation) {
-    query = composeQuery(query, 'translation', translation);
-  }
+  const variableToKeyItems = [
+    { key: 'combined_text', value: combinedText },
+    { key: 'division', value: division },
+    { key: 'end', value: end },
+    { key: 'ends_after', value: endsAfter },
+    { key: 'ends_before', value: endsBefore },
+    { key: 'include', value: include },
+    { key: 'in_language', value: inLanguage },
+    { key: 'is_free', value: isFree },
+    { key: 'keyword', value: keyword },
+    { key: 'keyword_AND', value: keywordAnd },
+    { key: 'keyword!', value: keywordNot },
+    { key: 'language', value: language },
+    { key: 'location', value: location },
+    { key: 'page', value: page },
+    { key: 'page_size', value: pageSize },
+    { key: 'publisher', value: publisher },
+    { key: 'sort', value: sort },
+    { key: 'start', value: start },
+    { key: 'starts_after', value: startsAfter },
+    { key: 'starts_before', value: startsBefore },
+    { key: 'super_event', value: superEvent },
+    { key: 'super_event_type', value: superEventType },
+    { key: 'text', value: text },
+    { key: 'translation', value: translation },
+  ];
+
+  const query = queryBuilder(variableToKeyItems);
 
   return `/event/${query}`;
 };
