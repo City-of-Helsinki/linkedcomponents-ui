c778c20ac49b0c86f13509a05da74d1975db019d
Jori Lindell <jori.lindell@gmail.com>
2020-11-12 19:19:11 +0200

    keywordSetsPathBuilder helper function

--- src/domain/keywordSet/__tests__/utils.test.ts
+++ src/domain/keywordSet/__tests__/utils.test.ts
@@ -1,4 +1,4 @@
-import { keywordSetPathBuilder } from '../utils';
+import { keywordSetPathBuilder, keywordSetsPathBuilder } from '../utils';
 
 describe('keywordSetPathBuilder function', () => {
   it('should build correct path', () => {
@@ -7,5 +7,15 @@ describe('keywordSetPathBuilder function', () => {
         args: { id: 'hel:123', include: ['include1', 'include2'] },
       })
     ).toBe('/keyword_set/hel:123/?include=include1,include2');
+  });
+});
+
+describe('keywordSetsPathBuilder function', () => {
+  it('should build correct path', () => {
+    expect(
+      keywordSetsPathBuilder({
+        args: { include: ['include1', 'include2'] },
+      })
+    ).toBe('/keyword_set/?include=include1,include2');
   });
 });
--- src/domain/keywordSet/utils.ts
+++ src/domain/keywordSet/utils.ts
@@ -1,8 +1,15 @@
-import { KeywordSetQueryVariables } from '../../generated/graphql';
+import {
+  KeywordSetQueryVariables,
+  KeywordSetsQueryVariables,
+} from '../../generated/graphql';
 import queryBuilder from '../../utils/queryBuilder';
 
 interface KeywordSetPathBuilderProps {
   args: KeywordSetQueryVariables;
+}
+
+interface KeywordSetsPathBuilderProps {
+  args: KeywordSetsQueryVariables;
 }
 
 export const keywordSetPathBuilder = ({ args }: KeywordSetPathBuilderProps) => {
@@ -12,4 +19,15 @@ export const keywordSetPathBuilder = ({ args }: KeywordSetPathBuilderProps) => {
   const query = queryBuilder(variableToKeyItems);
 
   return `/keyword_set/${id}/${query}`;
+};
+
+export const keywordSetsPathBuilder = ({
+  args,
+}: KeywordSetsPathBuilderProps) => {
+  const { include } = args;
+  const variableToKeyItems = [{ key: 'include', value: include }];
+
+  const query = queryBuilder(variableToKeyItems);
+
+  return `/keyword_set/${query}`;
 };
