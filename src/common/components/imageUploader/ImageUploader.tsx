f67408028b3f900ec03bcbd74513f67ba7739e8d
Jori Lindell <jori.lindell@gmail.com>
2020-12-10 19:06:55 +0200

    Change label to button to show upload file text correctly for screen readers

--- src/common/components/imageUploader/ImageUploader.tsx
+++ src/common/components/imageUploader/ImageUploader.tsx
@@ -1,14 +1,11 @@
 import classNames from 'classnames';
 import { IconDownload } from 'hds-react';
-import uniqueId from 'lodash/uniqueId';
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 import { toast } from 'react-toastify';
 
 import { ACCEPTED_IMAGE_TYPES, MAX_IMAGE_SIZE_MB } from '../../../constants';
 import styles from './imageUploader.module.scss';
-
-const generateUniqueId = (prefix: string) => `${prefix}-${uniqueId()}`;
 
 export const testIds = {
   input: 'image-uploader-file-input',
@@ -22,10 +19,6 @@ export interface ImageUploaderProps {
 const ImageUploader: React.FC<ImageUploaderProps> = ({ onChange }) => {
   const { t } = useTranslation();
   const fileInput = React.useRef<HTMLInputElement | null>(null);
-  const fileInputId = React.useMemo(
-    () => generateUniqueId('image-uploader-file-input'),
-    []
-  );
   const [hovered, setHovered] = React.useState(false);
 
   const handleFile = (file: File) => {
@@ -83,39 +76,31 @@ const ImageUploader: React.FC<ImageUploaderProps> = ({ onChange }) => {
     }
   };
 
-  const handleKeyDown = (e: React.KeyboardEvent) => {
-    /* istanbul ignore else  */
-    if (e.key === 'Enter') {
-      fileInput.current?.click();
-    }
+  const handleClick = () => {
+    fileInput.current?.click();
   };
 
   return (
     <div className={styles.imageUploader}>
-      <label
-        data-testid={testIds.label}
-        htmlFor={fileInputId}
-        className={classNames(styles.dropZone, { [styles.hover]: hovered })}
-        onDragOver={handleDragOver}
-        onDragLeave={handleDragLeave}
-        onDrop={handleDrop}
-        onKeyDown={handleKeyDown}
-        tabIndex={0}
-      >
-        <p>
-          <IconDownload />
-          {t('common.imageUploader.buttonDropImage')}
-        </p>
-      </label>
       <input
         data-testid={testIds.input}
-        id={fileInputId}
         ref={fileInput}
         type="file"
         onChange={handleChange}
         accept={'image/*'}
         hidden={true}
       />
+      <button
+        data-testid={testIds.label}
+        className={classNames(styles.dropZone, { [styles.hover]: hovered })}
+        onDragOver={handleDragOver}
+        onDragLeave={handleDragLeave}
+        onDrop={handleDrop}
+        onClick={handleClick}
+      >
+        <IconDownload aria-hidden={true} />
+        {t('common.imageUploader.buttonDropImage')}
+      </button>
     </div>
   );
 };
