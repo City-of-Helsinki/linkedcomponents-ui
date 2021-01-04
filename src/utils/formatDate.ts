08ba6f2d494bc8c3786891e405061977e2afd0d5
Jori Lindell <jori.lindell@gmail.com>
2021-01-04 14:43:29 +0200

    formatDate util function

---
+++ src/utils/formatDate.ts
@@ -0,0 +1,21 @@
+import { format } from 'date-fns';
+import { enGB as en, fi, sv } from 'date-fns/locale';
+
+import { Language } from '../types';
+
+const locales = { en, fi, sv };
+
+/**
+ * Format date string
+ * @param date
+ * @param dateFormat
+ * @returns {string}
+ */
+const formatDate = (
+  date: Date | null,
+  dateFormat = 'dd.MM.yyyy',
+  locale: Language = 'fi'
+): string =>
+  date ? format(date, dateFormat, { locale: locales[locale] }) : '';
+
+export default formatDate;
