27dd7f0f002eaac8582b95b53f129ec9823b05ca
Jori Lindell <jori.lindell@gmail.com>
2021-03-21 20:15:46 +0200

    Content for technology help pages

--- src/domain/help/pages/GeneralTechnologyPage.tsx
+++ src/domain/help/pages/GeneralTechnologyPage.tsx
@@ -1,7 +1,91 @@
 import React from 'react';
 
+import ExternalLink from '../../../common/components/externalLink/ExternalLink';
+import useLocale from '../../../hooks/useLocale';
+import { Language } from '../../../types';
+import PageWrapper from '../../app/layout/PageWrapper';
+
 const GeneralTechnologyPage = () => {
-  return <h1>TODO: General technology page</h1>;
+  const locale = useLocale();
+
+  const getContent = (locale: Language) => {
+    switch (locale) {
+      case 'en':
+        return (
+          <>
+            <h1>Technology</h1>
+            <p>
+              Linked Events is developed for the Django platform in Python and
+              Javascript programming languages and utilizes a PostgreSQL
+              database to capture data. Linked Events Admin is a React-based
+              application for event management and its and the platform's source
+              codes can be found in the Github version control at:
+            </p>
+            <p>
+              <ExternalLink href="https://github.com/City-of-Helsinki/linkedevents">
+                https://github.com/City-of-Helsinki/linkedevents
+              </ExternalLink>
+            </p>
+            <p>
+              <ExternalLink href="https://github.com/City-of-Helsinki/linkedcomponents-ui">
+                https://github.com/City-of-Helsinki/linkedcomponents-ui
+              </ExternalLink>
+            </p>
+          </>
+        );
+      case 'fi':
+        return (
+          <>
+            <h1>Teknologia</h1>
+            <p>
+              Linked Events on kehitetty Django alustalle Python ja Javascript
+              ohjelmointikielillä ja se hyödyntää PostgreSQL tietokantaa
+              tietojen taltioimiseen. Linked Events Admin on React-pohjainen
+              sovellus tapahtumien hallintaan ja sen sekä alustan lähdekoodit
+              löytyvät Github-versionhallinnasta osoitteista:
+            </p>
+            <p>
+              <ExternalLink href="https://github.com/City-of-Helsinki/linkedevents">
+                https://github.com/City-of-Helsinki/linkedevents
+              </ExternalLink>
+            </p>
+            <p>
+              <ExternalLink href="https://github.com/City-of-Helsinki/linkedcomponents-ui">
+                https://github.com/City-of-Helsinki/linkedcomponents-ui
+              </ExternalLink>
+            </p>
+          </>
+        );
+      case 'sv':
+        return (
+          <>
+            <h1>Teknologi</h1>
+            <p>
+              Linked Events är utvecklat för Django-plattformen i
+              programmeringsspråk Python och Javascript och använder en
+              PostgreSQL-databas för att fånga data. Linked Events Admin är en
+              React-baserad applikation för händelsehantering och dess och
+              plattformens källkod finns i Github-versionskontrollen på:
+            </p>
+            <p>
+              <ExternalLink href="https://github.com/City-of-Helsinki/linkedevents">
+                https://github.com/City-of-Helsinki/linkedevents
+              </ExternalLink>
+            </p>
+            <p>
+              <ExternalLink href="https://github.com/City-of-Helsinki/linkedcomponents-ui">
+                https://github.com/City-of-Helsinki/linkedcomponents-ui
+              </ExternalLink>
+            </p>
+          </>
+        );
+    }
+  };
+  return (
+    <PageWrapper title="helpPage.pageTitleTechnology">
+      {getContent(locale)}
+    </PageWrapper>
+  );
 };
 
 export default GeneralTechnologyPage;
--- src/domain/help/pages/ImageRightsPage.tsx
+++ src/domain/help/pages/ImageRightsPage.tsx
@@ -1,7 +1,66 @@
 import React from 'react';
 
+import useLocale from '../../../hooks/useLocale';
+import { Language } from '../../../types';
+import PageWrapper from '../../app/layout/PageWrapper';
+
 const ImageRightsPage = () => {
-  return <h1>TODO: Image rights page</h1>;
+  const locale = useLocale();
+
+  const getContent = (locale: Language) => {
+    switch (locale) {
+      case 'en':
+        return (
+          <>
+            <h1>Image rights</h1>
+            <p>
+              The City of Helsinki has all the rights to the images. If the
+              license of the image to be shared in the interface is marked
+              event_only, the image may only be used for information and
+              communication about the event of the image. Using images for this
+              purpose is free for the user. Use or transfer of the image for
+              other purposes is prohibited. When using images, it is essential
+              to mention the source and photographer of the images.
+            </p>
+          </>
+        );
+      case 'fi':
+        return (
+          <>
+            <h1>Kuvaoikeudet</h1>
+            <p>
+              Helsingin kaupungilla on kuviin kaikki oikeudet. Mikäli
+              rajapinnassa jaettavan kuvan tietojen kohdassa license on merkintä
+              event_only, saa kuvaa käyttää ainoastaan kuvan tapahtumaa
+              käsittelevään tiedotukseen ja viestintään. Kuvien käyttäminen
+              tähän tarkoitukseen on käyttäjälle ilmaista. Kuvan käyttö tai
+              siirto muihin tarkoituksiin on kielletty. Kuvia käytettäessä on
+              ehdottomasti mainittava kuvien lähde ja kuvaaja.
+            </p>
+          </>
+        );
+      case 'sv':
+        return (
+          <>
+            <h1>Bildrättigheter</h1>
+            <p>
+              Helsingfors stad har alla rättigheter till bilderna. Om licensen
+              för bilden som ska delas i gränssnittet är markerad endast
+              händelse får bilden endast användas för information och
+              kommunikation om bildens händelse. Att använda bilder för detta
+              ändamål är gratis för användaren. Användning eller överföring av
+              bilden för andra ändamål är förbjuden. När du använder bilder är
+              det viktigt att nämna bildens källa och graf.
+            </p>
+          </>
+        );
+    }
+  };
+  return (
+    <PageWrapper title="helpPage.pageTitleImageRights">
+      {getContent(locale)}
+    </PageWrapper>
+  );
 };
 
 export default ImageRightsPage;
