207b463c6ff1fb9cbdc611cd624180a927326633
Jori Lindell <jori.lindell@gmail.com>
2021-04-07 10:16:34 +0300

    Sentry DSN can be public so store it to .env

--- src/index.tsx
+++ src/index.tsx
@@ -8,7 +8,7 @@ import ReactDOM from 'react-dom';
 
 import App from './domain/app/App';
 
-if (process.env.REACT_APP_SENTRY_DSN) {
+if (process.env.REACT_APP_SENTRY_ENVIRONMENT) {
   Sentry.init({
     dsn: process.env.REACT_APP_SENTRY_DSN,
     environment: process.env.REACT_APP_SENTRY_ENVIRONMENT,
