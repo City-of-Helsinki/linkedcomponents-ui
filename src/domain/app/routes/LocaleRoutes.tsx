9f9bec5ef94bde74ef8a4790c7e7a03b63f8341f
Jori Lindell <jori.lindell@gmail.com>
2021-04-22 18:42:03 +0300

    Use React lazy load to reduce single file sizes

--- src/domain/app/routes/LocaleRoutes.tsx
+++ src/domain/app/routes/LocaleRoutes.tsx
@@ -4,16 +4,25 @@ import { Route, RouteComponentProps, Switch } from 'react-router';
 
 import { ROUTES } from '../../../constants';
 import { Language } from '../../../types';
-import EditEventPage from '../../event/EditEventPage';
-import EventSavedPage from '../../eventSaved/EventSavedPage';
-import EventSearchPage from '../../eventSearch/EventSearchPage';
-import LogoutPage from '.././../auth/logoutPage/LogoutPage';
-import CreateEventPage from '.././../event/CreateEventPage';
-import EventsPage from '.././../events/EventsPage';
-import LandingPage from '.././../landingPage/LandingPage';
-import NotFound from '.././../notFound/NotFound';
 import PageLayout from '../layout/PageLayout';
-import HelpPageRoutes from './HelpPageRoutes';
+
+const CreateEventPage = React.lazy(
+  () => import('.././../event/CreateEventPage')
+);
+const EditEventPage = React.lazy(() => import('../../event/EditEventPage'));
+const EventSavedPage = React.lazy(
+  () => import('../../eventSaved/EventSavedPage')
+);
+const EventSearchPage = React.lazy(
+  () => import('../../eventSearch/EventSearchPage')
+);
+const EventsPage = React.lazy(() => import('.././../events/EventsPage'));
+const HelpPageRoutes = React.lazy(() => import('./HelpPageRoutes'));
+const LandingPage = React.lazy(() => import('.././../landingPage/LandingPage'));
+const LogoutPage = React.lazy(
+  () => import('.././../auth/logoutPage/LogoutPage')
+);
+const NotFound = React.lazy(() => import('.././../notFound/NotFound'));
 
 interface Params {
   locale: Language;
@@ -36,47 +45,49 @@ const LocaleRoutes: React.FC<Props> = ({
 
   return (
     <PageLayout>
-      <Switch>
-        <Route
-          exact
-          path={getLocalePath(ROUTES.HOME)}
-          component={LandingPage}
-        />
-        <Route
-          exact
-          path={getLocalePath(ROUTES.CREATE_EVENT)}
-          component={CreateEventPage}
-        />
-        <Route
-          exact
-          path={getLocalePath(ROUTES.EVENT_SAVED)}
-          component={EventSavedPage}
-        />
-        <Route
-          exact
-          path={getLocalePath(ROUTES.EDIT_EVENT)}
-          component={EditEventPage}
-        />
-        <Route
-          exact
-          path={getLocalePath(ROUTES.EVENTS)}
-          component={EventsPage}
-        />
-        <Route
-          exact
-          path={getLocalePath(ROUTES.LOGOUT)}
-          component={LogoutPage}
-        />
-        <Route
-          exact
-          path={getLocalePath(ROUTES.SEARCH)}
-          component={EventSearchPage}
-        />
-        <Route path={getLocalePath(ROUTES.HELP)}>
-          <HelpPageRoutes locale={locale} />
-        </Route>
-        <Route component={NotFound} />
-      </Switch>
+      <React.Suspense fallback={<div style={{ minHeight: '100vh' }} />}>
+        <Switch>
+          <Route
+            exact
+            path={getLocalePath(ROUTES.HOME)}
+            component={LandingPage}
+          />
+          <Route
+            exact
+            path={getLocalePath(ROUTES.CREATE_EVENT)}
+            component={CreateEventPage}
+          />
+          <Route
+            exact
+            path={getLocalePath(ROUTES.EVENT_SAVED)}
+            component={EventSavedPage}
+          />
+          <Route
+            exact
+            path={getLocalePath(ROUTES.EDIT_EVENT)}
+            component={EditEventPage}
+          />
+          <Route
+            exact
+            path={getLocalePath(ROUTES.EVENTS)}
+            component={EventsPage}
+          />
+          <Route
+            exact
+            path={getLocalePath(ROUTES.LOGOUT)}
+            component={LogoutPage}
+          />
+          <Route
+            exact
+            path={getLocalePath(ROUTES.SEARCH)}
+            component={EventSearchPage}
+          />
+          <Route path={getLocalePath(ROUTES.HELP)}>
+            <HelpPageRoutes locale={locale} />
+          </Route>
+          <Route component={NotFound} />
+        </Switch>
+      </React.Suspense>
     </PageLayout>
   );
 };
