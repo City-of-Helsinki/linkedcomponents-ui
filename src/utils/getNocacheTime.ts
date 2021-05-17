94dca6d9f4c5e9cea41fe5f80fd6b725c3a37f8d
Jori Lindell <jori.lindell@gmail.com>
2021-05-17 11:30:59 +0300

    Change unix time to Date.now() when defining nocache time

--- src/utils/getNocacheTime.ts
+++ src/utils/getNocacheTime.ts
@@ -1,9 +1,7 @@
-import getUnixTime from 'date-fns/getUnixTime';
-
 import { TEST_NOCACHE_TIME } from '../constants';
 import isTestEnv from './isTestEnv';
 
 const getNocacheTime = (): number =>
-  isTestEnv ? TEST_NOCACHE_TIME : getUnixTime(new Date());
+  isTestEnv ? TEST_NOCACHE_TIME : Date.now();
 
 export default getNocacheTime;
