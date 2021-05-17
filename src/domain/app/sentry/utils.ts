0827b3f003b296a69b6acaf0d80ec9bcaa7218c6
Jori Lindell <jori.lindell@gmail.com>
2021-05-17 19:22:41 +0300

    Also stringify error when reporting error to sentry

--- src/domain/app/sentry/utils.ts
+++ src/domain/app/sentry/utils.ts
@@ -9,25 +9,29 @@ const reportError = ({
   message,
   user,
 }: {
-  data?: Record<string, unknown>;
+  data: { error: Record<string, unknown> } & Record<string, unknown>;
   location: H.Location;
   message: string;
   user?: UserFieldsFragment;
 }): string => {
+  const { error, ...restData } = data;
   const reportObject = {
     extra: {
       data: {
+        ...restData,
         currentUrl: window.location.href,
         location,
         timestamp: new Date(),
         user,
         userAsString: user ? JSON.stringify(user) : '',
         userAgent: navigator.userAgent,
-        ...data,
+        error,
+        errorAsString: JSON.stringify(error),
       },
       level: 'error',
     },
   };
+
   return Sentry.captureException(message, reportObject);
 };
 
