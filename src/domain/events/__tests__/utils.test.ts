e8093e12df107ca8816d193415eeaf602d2bb0a1
jorilindell <jori.lindell@gmail.com>
2021-05-27 14:57:03 +0300

    Default value for event type filtering (#51)

--- src/domain/events/__tests__/utils.test.ts
+++ src/domain/events/__tests__/utils.test.ts
@@ -7,72 +7,125 @@ import { eventsPathBuilder } from '../utils';
 
 describe('eventsPathBuilder function', () => {
   const cases: [EventsQueryVariables, string][] = [
-    [{ adminUser: true }, '/event/?admin_user=true'],
-    [{ combinedText: ['text1', 'text2'] }, '/event/?combined_text=text1,text2'],
-    [{ createdBy: 'me' }, '/event/?created_by=me'],
+    [
+      { adminUser: true },
+      '/event/?admin_user=true&event_type=General,Course,Volunteering',
+    ],
+    [
+      { combinedText: ['text1', 'text2'] },
+      '/event/?combined_text=text1,text2&event_type=General,Course,Volunteering',
+    ],
+    [
+      { createdBy: 'me' },
+      '/event/?created_by=me&event_type=General,Course,Volunteering',
+    ],
     [
       { division: ['division1', 'division2'] },
-      '/event/?division=division1,division2',
+      '/event/?division=division1,division2&event_type=General,Course,Volunteering',
     ],
-    [{ end: '2020-12-12' }, '/event/?end=2020-12-12'],
-    [{ endsAfter: '14' }, '/event/?ends_after=14'],
-    [{ endsBefore: '14' }, '/event/?ends_before=14'],
+    [
+      { end: '2020-12-12' },
+      '/event/?end=2020-12-12&event_type=General,Course,Volunteering',
+    ],
+    [
+      { endsAfter: '14' },
+      '/event/?ends_after=14&event_type=General,Course,Volunteering',
+    ],
+    [
+      { endsBefore: '14' },
+      '/event/?ends_before=14&event_type=General,Course,Volunteering',
+    ],
     [
       {
-        eventType: [
-          EventTypeId.Course,
-          EventTypeId.General,
-          EventTypeId.Volunteering,
-        ],
+        eventType: [EventTypeId.Course, EventTypeId.General],
       },
-      '/event/?event_type=Course,General,Volunteering',
+      '/event/?event_type=Course,General',
     ],
     [
       { include: ['include1', 'include2'] },
-      '/event/?include=include1,include2',
+      '/event/?event_type=General,Course,Volunteering&include=include1,include2',
     ],
-    [{ inLanguage: 'fi' }, '/event/?in_language=fi'],
-    [{ isFree: true }, '/event/?is_free=true'],
-    [{ isFree: false }, '/event/?is_free=false'],
+    [
+      { inLanguage: 'fi' },
+      '/event/?event_type=General,Course,Volunteering&in_language=fi',
+    ],
+    [
+      { isFree: true },
+      '/event/?event_type=General,Course,Volunteering&is_free=true',
+    ],
+    [
+      { isFree: false },
+      '/event/?event_type=General,Course,Volunteering&is_free=false',
+    ],
     [
       { keyword: ['keyword1', 'keyword2'] },
-      '/event/?keyword=keyword1,keyword2',
+      '/event/?event_type=General,Course,Volunteering&keyword=keyword1,keyword2',
     ],
     [
       { keywordAnd: ['keyword1', 'keyword2'] },
-      '/event/?keyword_AND=keyword1,keyword2',
+      '/event/?event_type=General,Course,Volunteering&keyword_AND=keyword1,keyword2',
     ],
     [
       { keywordNot: ['keyword1', 'keyword2'] },
-      '/event/?keyword!=keyword1,keyword2',
+      '/event/?event_type=General,Course,Volunteering&keyword!=keyword1,keyword2',
     ],
-    [{ language: 'fi' }, '/event/?language=fi'],
+    [
+      { language: 'fi' },
+      '/event/?event_type=General,Course,Volunteering&language=fi',
+    ],
     [
       { location: ['location1', 'location2'] },
-      '/event/?location=location1,location2',
+      '/event/?event_type=General,Course,Volunteering&location=location1,location2',
     ],
-    [{ page: 2 }, '/event/?page=2'],
-    [{ pageSize: 10 }, '/event/?page_size=10'],
+    [{ page: 2 }, '/event/?event_type=General,Course,Volunteering&page=2'],
+    [
+      { pageSize: 10 },
+      '/event/?event_type=General,Course,Volunteering&page_size=10',
+    ],
     [
       { publicationStatus: PublicationStatus.Draft },
-      '/event/?publication_status=draft',
+      '/event/?event_type=General,Course,Volunteering&publication_status=draft',
     ],
     [
       { publisher: ['publisher1', 'publisher2'] },
-      '/event/?publisher=publisher1,publisher2',
+      '/event/?event_type=General,Course,Volunteering&publisher=publisher1,publisher2',
     ],
-    [{ showAll: true }, '/event/?show_all=true'],
-    [{ sort: 'start' }, '/event/?sort=start'],
-    [{ start: '2020-12-12' }, '/event/?start=2020-12-12'],
-    [{ startsAfter: '14' }, '/event/?starts_after=14'],
-    [{ startsBefore: '14' }, '/event/?starts_before=14'],
-    [{ superEvent: 'hel:123' }, '/event/?super_event=hel:123'],
+    [
+      { showAll: true },
+      '/event/?event_type=General,Course,Volunteering&show_all=true',
+    ],
+    [
+      { sort: 'start' },
+      '/event/?event_type=General,Course,Volunteering&sort=start',
+    ],
+    [
+      { start: '2020-12-12' },
+      '/event/?event_type=General,Course,Volunteering&start=2020-12-12',
+    ],
+    [
+      { startsAfter: '14' },
+      '/event/?event_type=General,Course,Volunteering&starts_after=14',
+    ],
+    [
+      { startsBefore: '14' },
+      '/event/?event_type=General,Course,Volunteering&starts_before=14',
+    ],
+    [
+      { superEvent: 'hel:123' },
+      '/event/?event_type=General,Course,Volunteering&super_event=hel:123',
+    ],
     [
       { superEventType: ['type1', 'type2'] },
-      '/event/?super_event_type=type1,type2',
+      '/event/?event_type=General,Course,Volunteering&super_event_type=type1,type2',
     ],
-    [{ text: 'text' }, '/event/?text=text'],
-    [{ translation: 'fi' }, '/event/?translation=fi'],
+    [
+      { text: 'text' },
+      '/event/?event_type=General,Course,Volunteering&text=text',
+    ],
+    [
+      { translation: 'fi' },
+      '/event/?event_type=General,Course,Volunteering&translation=fi',
+    ],
   ];
 
   it.each(cases)(
