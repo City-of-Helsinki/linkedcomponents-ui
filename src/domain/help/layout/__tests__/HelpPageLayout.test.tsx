2c4776012f176f7bcdd36b0116e5196ae663a984
Jori Lindell <jori.lindell@gmail.com>
2022-01-06 22:50:24 +0200

    Replace internal SideNavigation component with HDS SideNavigation

--- src/domain/help/layout/__tests__/HelpPageLayout.test.tsx
+++ src/domain/help/layout/__tests__/HelpPageLayout.test.tsx
@@ -1,27 +1,60 @@
 import React from 'react';
 
 import { ROUTES } from '../../../../constants';
-import { configure, render, screen } from '../../../../utils/testUtils';
+import {
+  configure,
+  render,
+  screen,
+  userEvent,
+  waitFor,
+} from '../../../../utils/testUtils';
 import HelpPageLayout from '../HelpPageLayout';
 
 configure({ defaultHidden: true });
 
-test.each([`/fi${ROUTES.INSTRUCTIONS_CONTROL_PANEL}`])(
-  'should render help page layout, route %p',
-  async (route) => {
-    render(<HelpPageLayout>Content</HelpPageLayout>, { routes: [route] });
+const route = `/fi${ROUTES.INSTRUCTIONS_CONTROL_PANEL}`;
+const renderComponent = () =>
+  render(<HelpPageLayout>Content</HelpPageLayout>, {
+    routes: [route],
+  });
 
-    screen.getByRole('link', { name: 'Ohjeet' });
-    screen.getByRole('link', { name: 'Teknologia' });
-    screen.getByRole('link', { name: 'Tuki' });
-    screen.getByRole('link', { name: 'Palvelun ominaisuudet' });
+test('should render help page layout', async () => {
+  renderComponent();
 
-    // Instruction sub-levels should be visible as well. Test only 1 of them to improve performace
-    screen.getByRole('link', { name: 'Hallintapaneeli', hidden: false });
+  screen.getByRole('button', { name: 'Ohjeet' });
+  screen.getByRole('button', { name: 'Teknologia' });
+  screen.getByRole('button', { name: 'Tuki' });
+  screen.getByRole('link', { name: 'Palvelun ominaisuudet' });
 
-    // Other sub-levels shouldn't be visible
-    expect(
-      screen.queryByRole('link', { name: 'Ota yhteyttä', hidden: false })
-    ).not.toBeInTheDocument();
-  }
-);
+  // Instruction sub-levels should be visible as well. Test only 1 of them to improve performace
+  screen.getByRole('link', { name: 'Hallintapaneeli', hidden: false });
+
+  // Other sub-levels shouldn't be visible
+  expect(
+    screen.queryByRole('link', { name: 'Ota yhteyttä', hidden: false })
+  ).not.toBeInTheDocument();
+});
+
+test('should router to features help page', async () => {
+  const { history } = renderComponent();
+
+  const featuresLink = screen.getByRole('link', {
+    name: 'Palvelun ominaisuudet',
+  });
+  userEvent.click(featuresLink);
+
+  await waitFor(() =>
+    expect(history.location.pathname).toBe('/fi/help/features')
+  );
+});
+
+test('should router to platform help page', async () => {
+  const { history } = renderComponent();
+
+  const platformLink = screen.getByRole('link', { name: 'Alusta' });
+  userEvent.click(platformLink);
+
+  await waitFor(() =>
+    expect(history.location.pathname).toBe('/fi/help/instructions/platform')
+  );
+});
