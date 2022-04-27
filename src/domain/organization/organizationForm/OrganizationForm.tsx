e599364ee885d4d70d32a21c5b1e85f128027639
Jori Lindell <jori.lindell@gmail.com>
2022-04-27 17:25:17 +0300

    Disable organization form fields if user has insufficient rights to edit/create organization

--- src/domain/organization/organizationForm/OrganizationForm.tsx
+++ src/domain/organization/organizationForm/OrganizationForm.tsx
@@ -41,12 +41,14 @@ import {
 } from '../constants';
 import CreateButtonPanel from '../createButtonPanel/CreateButtonPanel';
 import EditButtonPanel from '../editButtonPanel/EditButtonPanel';
+import useOrganizationAncestors from '../hooks/useOrganizationAncestors';
 import useOrganizationInternalTypeOptions from '../hooks/useOrganizationInternalTypeOptions';
 import useOrganizationServerErrors from '../hooks/useOrganizationServerErrors';
 import useOrganizationUpdateActions from '../hooks/useOrganizationUpdateActions';
 import OrganizationAuthenticationNotification from '../organizationAuthenticationNotification/OrganizationAuthenticationNotification';
 import { OrganizationFormFields } from '../types';
 import {
+  checkCanUserDoAction,
   clearOrganizationsQueries,
   getOrganizationInitialValues,
   getOrganizationPayload,
@@ -68,6 +70,18 @@ const OrganizationForm: React.FC<OrganizationFormProps> = ({
   const internalTypeOptions = useOrganizationInternalTypeOptions();
   const apolloClient = useApolloClient() as ApolloClient<NormalizedCacheObject>;
   const { user } = useUser();
+  const { organizationAncestors } = useOrganizationAncestors(
+    organization?.id ?? ''
+  );
+
+  const isEditingAllowed = checkCanUserDoAction({
+    action: organization
+      ? ORGANIZATION_ACTIONS.UPDATE
+      : ORGANIZATION_ACTIONS.CREATE,
+    id: organization?.id ?? '',
+    organizationAncestors,
+    user,
+  });
 
   const goToOrganizationsPage = () => {
     navigate(`/${locale}${ROUTES.ORGANIZATIONS}`);
@@ -149,7 +163,7 @@ const OrganizationForm: React.FC<OrganizationFormProps> = ({
       validateOnMount
       validateOnBlur={true}
       validateOnChange={true}
-      validationSchema={organizationSchema}
+      validationSchema={isEditingAllowed && organizationSchema}
     >
       {({ setErrors, setTouched, values }) => {
         const clearErrors = () => setErrors({});
@@ -199,40 +213,55 @@ const OrganizationForm: React.FC<OrganizationFormProps> = ({
 
             <FormRow className={styles.borderInMobile}>
               <Field
-                className={styles.alignedInputWithFullBorder}
+                className={styles.alignedInput}
                 component={TextInputField}
                 label={t(`organization.form.labelId`)}
                 name={ORGANIZATION_FIELDS.ID}
                 readOnly
               />
             </FormRow>
-            <FormRow className={organization ? styles.borderInMobile : ''}>
+            <FormRow>
               <Field
                 className={styles.alignedSelect}
                 component={SingleDataSourceSelectorField}
-                disabled={!!organization}
+                disabled={!isEditingAllowed || !!organization}
                 label={t(`organization.form.labelDataSource`)}
                 name={ORGANIZATION_FIELDS.DATA_SOURCE}
                 required
                 showOnlyUserEditable={true}
               />
             </FormRow>
-            <FormRow className={organization ? styles.borderInMobile : ''}>
+            <FormRow
+              className={
+                !isEditingAllowed || organization ? styles.borderInMobile : ''
+              }
+            >
               <Field
-                className={styles.alignedInput}
+                className={
+                  /* istanbul ignore next */
+                  isEditingAllowed
+                    ? styles.alignedInput
+                    : styles.alignedInputWithFullBorder
+                }
                 component={TextInputField}
                 label={t(`organization.form.labelOriginId`)}
                 name={ORGANIZATION_FIELDS.ORIGIN_ID}
-                readOnly={!!organization}
+                readOnly={!isEditingAllowed || !!organization}
                 required
               />
             </FormRow>
-            <FormRow>
+            <FormRow
+              className={
+                /* istanbul ignore next */
+                !isEditingAllowed ? styles.borderInMobile : ''
+              }
+            >
               <Field
                 className={styles.alignedInput}
                 component={TextInputField}
                 label={t(`organization.form.labelName`)}
                 name={ORGANIZATION_FIELDS.NAME}
+                readOnly={!isEditingAllowed}
               />
             </FormRow>
             <FormRow>
@@ -240,6 +269,7 @@ const OrganizationForm: React.FC<OrganizationFormProps> = ({
                 className={styles.alignedSelect}
                 clearable
                 component={UserSelectorField}
+                disabled={!isEditingAllowed}
                 label={t(`organization.form.labelAdminUsers`)}
                 name={ORGANIZATION_FIELDS.ADMIN_USERS}
               />
@@ -249,6 +279,7 @@ const OrganizationForm: React.FC<OrganizationFormProps> = ({
                 className={styles.alignedSelect}
                 clearable
                 component={UserSelectorField}
+                disabled={!isEditingAllowed}
                 label={t(`organization.form.labelRegularUsers`)}
                 name={ORGANIZATION_FIELDS.REGULAR_USERS}
               />
@@ -257,53 +288,62 @@ const OrganizationForm: React.FC<OrganizationFormProps> = ({
               <Field
                 className={styles.alignedSelect}
                 component={SingleSelectField}
+                disabled={!isEditingAllowed || !!organization}
                 label={t(`organization.form.labelInternalType`)}
                 options={internalTypeOptions}
                 name={ORGANIZATION_FIELDS.INTERNAL_TYPE}
-                disabled={!!organization}
               />
             </FormRow>
-            <FormRow className={organization ? styles.borderInMobile : ''}>
+            <FormRow>
               <Field
                 className={styles.alignedSelect}
                 clearable
                 component={SingleOrganizationClassSelectorField}
+                disabled={!isEditingAllowed || !!organization}
                 label={t(`organization.form.labelClassification`)}
                 name={ORGANIZATION_FIELDS.CLASSIFICATION}
-                disabled={!!organization}
               />
             </FormRow>
-            <FormRow className={organization ? styles.borderInMobile : ''}>
+            <FormRow
+              className={
+                !isEditingAllowed || organization ? styles.borderInMobile : ''
+              }
+            >
               <Field
                 className={styles.alignedInputWithFullBorder}
                 component={DatepickerField}
                 label={t(`organization.form.labelFoundingDate`)}
                 name={ORGANIZATION_FIELDS.FOUNDING_DATE}
-                readOnly={!!organization}
+                readOnly={!isEditingAllowed || !!organization}
               />
             </FormRow>
-            <FormRow className={organization ? styles.borderInMobile : ''}>
+            <FormRow
+              className={
+                !isEditingAllowed || organization ? styles.borderInMobile : ''
+              }
+            >
               <Field
                 className={styles.alignedInput}
                 component={DatepickerField}
                 label={t(`organization.form.labelDissolutionDate`)}
                 name={ORGANIZATION_FIELDS.DISSOLUTION_DATE}
-                readOnly={!!organization}
+                readOnly={!isEditingAllowed || !!organization}
               />
             </FormRow>
             <FormRow>
               <Field
                 className={styles.alignedSelect}
                 component={SingleOrganizationSelectorField}
+                disabled={!isEditingAllowed || !!organization}
                 label={t(`organization.form.labelParentOrganization`)}
                 name={ORGANIZATION_FIELDS.PARENT_ORGANIZATION}
-                disabled={!!organization}
               />
             </FormRow>
             <FormRow>
               <Field
                 className={styles.alignedSelect}
                 component={SingleOrganizationSelectorField}
+                disabled={!isEditingAllowed}
                 label={t(`organization.form.labelReplacedBy`)}
                 name={ORGANIZATION_FIELDS.REPLACED_BY}
               />
