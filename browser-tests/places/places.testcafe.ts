be4518cea08d86b979523752a37006630796dce7
Jori Lindell <jori.lindell@gmail.com>
2022-05-23 15:30:12 +0300

    Remove if statement from places test file to test does it effect to failing acceptance tests

--- browser-tests/places/places.testcafe.ts
+++ browser-tests/places/places.testcafe.ts
@@ -4,7 +4,6 @@ import { RequestLogger } from 'testcafe';
 import { SUPPORTED_LANGUAGES } from '../../src/constants';
 import { findCookieConsentModal } from '../cookieConsentModal/cookieConsentModal.components';
 import { getPlaces } from '../data/placeData';
-import { isFeatureEnabled } from '../utils/featureFlag.utils';
 import { isLocalized } from '../utils/place.utils';
 import { getRandomSentence } from '../utils/random.utils';
 import { requestLogger } from '../utils/requestLogger';
@@ -32,35 +31,32 @@ fixture('Places page')
   })
   .requestHooks([requestLogger, placesLogger]);
 
-if (isFeatureEnabled('SHOW_ADMIN')) {
-  test('Create place button works', async (t) => {
-    const cookieConsentModal = await findCookieConsentModal(t);
-    await cookieConsentModal.actions.acceptAllCookies();
+test('Create place button works', async (t) => {
+  console.log(process.env);
+  const cookieConsentModal = await findCookieConsentModal(t);
+  await cookieConsentModal.actions.acceptAllCookies();
 
-    const keywordsPage = await getPlacesPage(t);
-    await keywordsPage.actions.clickCreatePlaceButton();
-    await urlUtils.expectations.urlChangedToCreatePlacePage();
-  });
+  const placesPage = await getPlacesPage(t);
+  await placesPage.actions.clickCreatePlaceButton();
+  await urlUtils.expectations.urlChangedToCreatePlacePage();
+});
 
-  test('Search url by place name shows place row data for a place', async (t) => {
-    const cookieConsentModal = await findCookieConsentModal(t);
-    await cookieConsentModal.actions.acceptAllCookies();
+test('Search url by place name shows place row data for a place', async (t) => {
+  const cookieConsentModal = await findCookieConsentModal(t);
+  await cookieConsentModal.actions.acceptAllCookies();
 
-    const placesPage = await getPlacesPage(t);
-    await placesPage.pageIsLoaded();
+  const placesPage = await getPlacesPage(t);
+  await placesPage.pageIsLoaded();
 
-    const locale = SUPPORTED_LANGUAGES.FI;
-    const places = await getPlaces(t, placesLogger);
-    const [place] = places.filter((place) => isLocalized(place, locale));
+  const locale = SUPPORTED_LANGUAGES.FI;
+  const places = await getPlaces(t, placesLogger);
+  const [place] = places.filter((place) => isLocalized(place, locale));
 
-    await urlUtils.actions.navigateToPlacesUrl(
-      getRandomSentence(place.name.fi)
-    );
+  await urlUtils.actions.navigateToPlacesUrl(getRandomSentence(place.name.fi));
 
-    const searchResults = await placesPage.findSearchResultList();
-    const placeRow = await searchResults.placeRow(place);
+  const searchResults = await placesPage.findSearchResultList();
+  const placeRow = await searchResults.placeRow(place);
 
-    await placeRow.actions.clickPlaceRow();
-    await urlUtils.expectations.urlChangedToPlacePage(place);
-  });
-}
+  await placeRow.actions.clickPlaceRow();
+  await urlUtils.expectations.urlChangedToPlacePage(place);
+});
