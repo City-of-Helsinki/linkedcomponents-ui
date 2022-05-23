666bd4e97af3d82cdcbd43a7fdb3027b342635cd
Jori Lindell <jori.lindell@gmail.com>
2022-05-23 22:12:07 +0300

    Test is only admin routes blocked

--- browser-tests/places/places.testcafe.ts
+++ browser-tests/places/places.testcafe.ts
@@ -4,6 +4,7 @@ import { RequestLogger } from 'testcafe';
 import { SUPPORTED_LANGUAGES } from '../../src/constants';
 import { findCookieConsentModal } from '../cookieConsentModal/cookieConsentModal.components';
 import { getPlaces } from '../data/placeData';
+import { isFeatureEnabled } from '../utils/featureFlag.utils';
 import { isLocalized } from '../utils/place.utils';
 import { getRandomSentence } from '../utils/random.utils';
 import { requestLogger } from '../utils/requestLogger';
@@ -20,7 +21,7 @@ const placesLogger = RequestLogger(new RegExp(LINKED_EVENTS_URL), {
 let urlUtils: ReturnType<typeof getUrlUtils>;
 
 fixture('Places page')
-  .page(getEnvUrl('/fi/search'))
+  .page(getEnvUrl('/fi/admin-test/places'))
   .beforeEach(async (t) => {
     clearDataToPrintOnFailure(t);
     urlUtils = getUrlUtils(t);
@@ -31,31 +32,35 @@ fixture('Places page')
   })
   .requestHooks([requestLogger, placesLogger]);
 
-test('Create place button works', async (t) => {
-  const cookieConsentModal = await findCookieConsentModal(t);
-  await cookieConsentModal.actions.acceptAllCookies();
+if (isFeatureEnabled('SHOW_ADMIN')) {
+  test('Create place button works', async (t) => {
+    const cookieConsentModal = await findCookieConsentModal(t);
+    await cookieConsentModal.actions.acceptAllCookies();
 
-  const placesPage = await getPlacesPage(t);
-  await placesPage.actions.clickCreatePlaceButton();
-  await urlUtils.expectations.urlChangedToCreatePlacePage();
-});
+    const keywordsPage = await getPlacesPage(t);
+    await keywordsPage.actions.clickCreatePlaceButton();
+    await urlUtils.expectations.urlChangedToCreatePlacePage();
+  });
 
-test('Search url by place name shows place row data for a place', async (t) => {
-  const cookieConsentModal = await findCookieConsentModal(t);
-  await cookieConsentModal.actions.acceptAllCookies();
+  test('Search url by place name shows place row data for a place', async (t) => {
+    const cookieConsentModal = await findCookieConsentModal(t);
+    await cookieConsentModal.actions.acceptAllCookies();
 
-  const placesPage = await getPlacesPage(t);
-  await placesPage.pageIsLoaded();
+    const placesPage = await getPlacesPage(t);
+    await placesPage.pageIsLoaded();
 
-  const locale = SUPPORTED_LANGUAGES.FI;
-  const places = await getPlaces(t, placesLogger);
-  const [place] = places.filter((place) => isLocalized(place, locale));
+    const locale = SUPPORTED_LANGUAGES.FI;
+    const places = await getPlaces(t, placesLogger);
+    const [place] = places.filter((place) => isLocalized(place, locale));
 
-  await urlUtils.actions.navigateToPlacesUrl(getRandomSentence(place.name.fi));
+    await urlUtils.actions.navigateToPlacesUrl(
+      getRandomSentence(place.name.fi)
+    );
 
-  const searchResults = await placesPage.findSearchResultList();
-  const placeRow = await searchResults.placeRow(place);
+    const searchResults = await placesPage.findSearchResultList();
+    const placeRow = await searchResults.placeRow(place);
 
-  await placeRow.actions.clickPlaceRow();
-  await urlUtils.expectations.urlChangedToPlacePage(place);
-});
+    await placeRow.actions.clickPlaceRow();
+    await urlUtils.expectations.urlChangedToPlacePage(place);
+  });
+}
