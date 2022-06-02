834a6a0f2081462ba0e37c44b91c921afe01766e
Jori Lindell <jori.lindell@gmail.com>
2022-06-02 15:31:45 +0300

    File path to file import

--- src/domain/events/eventCard/__tests__/PublicationStatus.test.tsx
+++ src/domain/events/eventCard/__tests__/PublicationStatus.test.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 import { PublicationStatus as PublicationStatusEnum } from '../../../../generated/graphql';
 import { render, screen } from '../../../../utils/testUtils';
-import PublicationStatus from '../PublicationStatus';
+import PublicationStatus from '../publicationStatus/PublicationStatus';
 
 test('should render correct text when publication status is draft', () => {
   render(<PublicationStatus publicationStatus={PublicationStatusEnum.Draft} />);
