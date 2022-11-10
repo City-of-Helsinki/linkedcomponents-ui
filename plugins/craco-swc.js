f868d8d7fea07caf468011ca828a447f7f7988cf
Jori Lindell <jori.lindell@gmail.com>
2022-11-10 14:48:20 +0200

    Replace babel-loader with swc-loader

--- plugins/craco-swc.js
+++ plugins/craco-swc.js
@@ -3,8 +3,62 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 const path = require('path');
 const fs = require('fs');
+const { loaderByName, removeLoaders, addAfterLoader } = require('@craco/craco');
 
 module.exports = {
+  /**
+   * To process the js/ts files we replace the babel-loader with the swc-loader
+   */
+  overrideWebpackConfig: ({
+    webpackConfig,
+    pluginOptions,
+    context: { paths },
+  }) => {
+    const useTypeScript = fs.existsSync(paths.appTsConfig);
+    const appSwcConfig = path.resolve(paths.appPath, '.swcrc');
+    const useSwcConfig = fs.existsSync(appSwcConfig);
+
+    // add swc-loader
+    addAfterLoader(webpackConfig, loaderByName('babel-loader'), {
+      test: /\.(js|mjs|jsx|ts|tsx)$/,
+      include: paths.appSrc,
+      loader: require.resolve('swc-loader'),
+      options:
+        pluginOptions && pluginOptions.swcLoaderOptions
+          ? pluginOptions.swcLoaderOptions
+          : useSwcConfig
+          ? // If there is an .swcrc file at the root of the project we pass undefined
+            // That way swc will use the .swcrc config
+            undefined
+          : {
+              jsc: {
+                target: 'es2015',
+                externalHelpers: true,
+                transform: {
+                  react: {
+                    runtime: 'automatic',
+                  },
+                },
+                parser: useTypeScript
+                  ? {
+                      syntax: 'typescript',
+                      tsx: true,
+                      dynamicImport: true,
+                    }
+                  : {
+                      syntax: 'ecmascript',
+                      jsx: true,
+                      dynamicImport: true,
+                    },
+              },
+            },
+    });
+
+    // remove the babel loaders
+    removeLoaders(webpackConfig, loaderByName('babel-loader'));
+
+    return webpackConfig;
+  },
   /**
    * To process the js/ts files we replace the babel-loader with the swc jest loader
    */
