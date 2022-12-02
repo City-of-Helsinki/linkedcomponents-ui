e0a38089cd7c3f10588756ef22efce8ffbf9fa41
Jori Lindell <jori.lindell@gmail.com>
2022-12-02 11:20:42 +0200

    Remove extra onClick function from NumberInputField component

--- src/common/components/formFields/numberInputField/NumberInputField.tsx
+++ src/common/components/formFields/numberInputField/NumberInputField.tsx
@@ -13,7 +13,6 @@ const NumberInputField: React.FC<Props> = ({
   form,
   ...rest
 }) => {
-  const ref = React.useRef<HTMLInputElement>(null);
   const { t } = useTranslation();
   const [, { touched, error }] = useField(name);
 
@@ -26,18 +25,10 @@ const NumberInputField: React.FC<Props> = ({
         // even when minus/plus onBlur is called
         onBlur({ target: { id: name, value } });
       }}
-      onClick={() => {
-        // TODO: Remove this when HDS NumberInput calls onChange
-        // after clicking minus/plus button
-        if (ref.current?.value !== value) {
-          onChange({ target: { id: name, value: Number(ref.current?.value) } });
-        }
-      }}
     >
       <NumberInput
         {...rest}
         {...field}
-        ref={ref}
         id={name}
         name={name}
         errorText={errorText}
