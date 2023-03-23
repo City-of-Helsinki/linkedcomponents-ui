fce3a698e72b31db272ed70069f0ee920018d295
Jori Lindell <jori.lindell@gmail.com>
2023-03-23 18:41:53 +0200

    Support filtering image table by id and name columns

--- src/domain/images/constants.ts
+++ src/domain/images/constants.ts
@@ -1,6 +1,10 @@
 export enum IMAGE_SORT_OPTIONS {
+  ID = 'id',
+  ID_DESC = '-id',
   LAST_MODIFIED_TIME = 'last_modified_time',
   LAST_MODIFIED_TIME_DESC = '-last_modified_time',
+  NAME = 'name',
+  NAME_DESC = '-name',
 }
 
 export const DEFAULT_IMAGE_SORT = IMAGE_SORT_OPTIONS.LAST_MODIFIED_TIME_DESC;
