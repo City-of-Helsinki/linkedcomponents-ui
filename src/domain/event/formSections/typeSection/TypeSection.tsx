ee9c6fd17820fbd37162b134d9855512209e0935
mikkojamG <mikko.jamia@gofore.com>
2023-06-13 08:20:25 +0300

    TypeSection differentiate text for event type

--- src/domain/event/formSections/typeSection/TypeSection.tsx
+++ src/domain/event/formSections/typeSection/TypeSection.tsx
@@ -128,11 +128,11 @@ const TypeSection: React.FC<TypeSectionProps> = ({
               label={t('event.form.notificationTitlePublication')}
               type="info"
             >
-              <p>{t('event.form.infoTextPublication.paragraph1')}</p>
+              <p>{t(`event.form.infoTextPublication.paragraph1.${type}`)}</p>
               <p>{t('event.form.infoTextPublication.paragraph2')}</p>
               <PublicationListLinks links={PUBLICATION_LIST_LINKS[type]} />
               <p>{t('event.form.infoTextPublication.paragraph3')}</p>
-              <p>{t('event.form.infoTextPublication.paragraph4')}</p>
+              <p>{t(`event.form.infoTextPublication.paragraph4.${type}`)}</p>
               <p>{t('event.form.infoTextPublication.paragraph5')}</p>
             </Notification>
           </>
