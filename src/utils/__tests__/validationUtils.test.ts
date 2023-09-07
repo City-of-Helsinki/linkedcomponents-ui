15f2d6e2431cd2dc4597a32ccf1825348bd2da7e
Jori Lindell <jori.lindell@gmail.com>
2023-09-07 14:38:09 +0300

    Improve phone number validation to allow e.g. +358 44 1234 567 which is used as placeholder text

--- src/utils/__tests__/validationUtils.test.ts
+++ src/utils/__tests__/validationUtils.test.ts
@@ -1,5 +1,10 @@
 /* eslint-disable max-len */
-import { getErrorText, isValidDateText, isValidUrl } from '../validationUtils';
+import {
+  getErrorText,
+  isValidDateText,
+  isValidPhoneNumber,
+  isValidUrl,
+} from '../validationUtils';
 
 describe('getErrorText', () => {
   it('should return error text', () => {
@@ -44,4 +49,42 @@ describe('isValidUrl', () => {
       )
     ).toBe(true);
   });
+});
+
+describe('isValidPhoneNumber function', () => {
+  const validCases: string[][] = [
+    ['0441234567'],
+    ['044 1234 567'],
+    ['044 123 4567'],
+    ['044 1234567'],
+    ['+358441234567'],
+    ['+358 44 123 4567'],
+    ['+358 44 1234 567'],
+    ['+358 441234567'],
+  ];
+  test.each(validCases)(
+    'should return true value if phone number is valid, %p',
+    async (phoneNumber) => {
+      expect(isValidPhoneNumber(phoneNumber)).toBe(true);
+    }
+  );
+
+  const invalidCases: string[][] = [
+    ['044  1234 56x'],
+    ['044  123x 567'],
+    ['04x  1234 567'],
+    ['044  1234 567'],
+    ['044 123  4567'],
+    ['044  1234567'],
+    ['+358  44 123 4567'],
+    ['+358 44  123 4567'],
+    ['+358 44 1234  567'],
+    ['+358  441234567'],
+  ];
+  test.each(invalidCases)(
+    'should return false value if phone number is invalid, %p',
+    async (phoneNumber) => {
+      expect(isValidPhoneNumber(phoneNumber)).toBe(false);
+    }
+  );
 });
