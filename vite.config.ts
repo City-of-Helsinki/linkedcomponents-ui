6af55e978c3cda43806c5ff89e231bc4f2a5b05c
Jori Lindell <jori.lindell@gmail.com>
2023-10-06 19:32:38 +0300

    Configure eslint

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
+import eslint from 'vite-plugin-eslint';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   base: '/',
-  plugins: [react()],
+  plugins: [react(), eslint()],
   build: {
     outDir: '../build',
   },
