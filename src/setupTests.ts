fbbec52e22130426c5438df7f92e78b1ddd6a747
Jori Lindell <jori.lindell@gmail.com>
2023-10-09 19:26:00 +0300

    Change jest to vitest
    - Configuration for the vitest
    - Remove all jest related packages
    - Add global vitest TypeScript types. Remove **/__mocks__/* from tsconfig.json excludes array to make this to work. This change requires to move some dev dependencies to dependencies
    - Configure coverage reports and junit report output file path
    - Replace jest-axe with vitest-axe

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,15 +1,20 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable no-console */
-import 'jest-axe/extend-expect';
+
 import './test/testI18nInit';
 // jest-dom adds custom jest matchers for asserting on DOM nodes.
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
-import '@testing-library/jest-dom';
-import 'jest-localstorage-mock';
+import '@testing-library/jest-dom/vitest';
+import 'vitest-axe/extend-expect';
+
+import { expect, vi } from 'vitest';
+import * as matchers from 'vitest-axe/matchers';
+expect.extend(matchers);
 
 // Mock scrollTo function
-window.scrollTo = jest.fn();
+window.scrollTo = vi.fn<any>();
 
 const originalWarn = console.warn.bind(console.warn);
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
@@ -33,8 +38,6 @@ console.warn = (msg: any, ...optionalParams: any[]) => {
     originalWarn(msg, ...optionalParams)
   );
 };
-
-jest.setTimeout(1000000);
 
 import.meta.env.REACT_APP_ENABLE_EXTERNAL_USER_EVENTS = 'true';
 import.meta.env.REACT_APP_MAINTENANCE_SHOW_NOTIFICATION = 'false';
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
-import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import eslint from 'vite-plugin-eslint';
+// eslint-disable-next-line import/no-unresolved
+import { coverageConfigDefaults, defineConfig } from 'vitest/config';
 
 export default defineConfig({
   base: '/',
@@ -14,5 +15,70 @@ export default defineConfig({
   },
   preview: {
     port: 3000,
+  },
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/setupTests.ts',
+    css: {
+      modules: {
+        classNameStrategy: 'non-scoped',
+      },
+    },
+    reporters: ['verbose', 'junit'],
+    outputFile: {
+      junit: './jest-report.xml',
+    },
+    coverage: {
+      reporter: ['clover', 'json', 'lcov', 'text', 'cobertura'],
+      include: ['src/**/*'],
+      exclude: [
+        ...coverageConfigDefaults.exclude,
+        '**/__mocks__/**',
+        '**/__snapshots__/**',
+        'build/**',
+        '**/constants.ts',
+        '**/types.ts',
+        '**/mutation.ts',
+        '**/query.ts',
+        '.*.d.ts',
+        'src/index.tsx',
+        'src/common/components/imageUploader/utils.ts',
+        'src/common/components/imageUploader/imageCropper/ImageCropper.tsx',
+        'src/common/components/menuDropdown/menu/Menu.tsx',
+        'src/common/components/table/bodyRow/BodyRow.tsx',
+        'src/common/components/table/sortingHeaderCell/SortingHeaderCell.tsx',
+        'src/common/components/table/tableBody/TableBody.tsx',
+        'src/common/components/table/tableContainer/TableContainer.tsx',
+        'src/common/components/map/',
+        'src/domain/app/apollo/apolloClient.ts',
+        'src/domain/app/apollo/clearCacheUtils.ts',
+        'src/domain/app/apollo/utils.ts',
+        'src/domain/app/hooks/usePageSettings.ts',
+        'src/domain/app/i18n/i18nInit.ts',
+        'src/domain/app/sentry/utils.ts',
+        'src/domain/app/theme/Theme.tsx',
+        'src/domain/auth/userManager.ts',
+        'src/domain/auth/callbackComponent/CallbackComponent.tsx',
+        'src/domain/auth/hooks/useAuth.ts',
+        'src/domain/auth/hooks/useApiToken.ts',
+        'src/domain/auth/oidcCallback/OidcCallback.tsx',
+        'src/domain/auth/silentCallback/SilentCallback.tsx',
+        'src/domain/event/formSections/timeSection/hooks/useTimeSectionContext.ts',
+        'src/domain/event/formSections/timeSection/TimeSectionContext.tsx',
+        'src/domain/help/pages/DocumentationPage.tsx',
+        'src/domain/organizations/organizationsTable/OrganizationsTableContext.tsx',
+        'src/generated/graphql.tsx',
+        'src/icons/',
+        'src/test/',
+        'src/utils/mockAuthContextValue.ts',
+        'src/utils/mockDataUtils.ts',
+        'src/utils/getPageHeaderHeight.ts',
+        'src/utils/testUtils.tsx',
+        'src/utils/typescript.ts',
+      ],
+      provider: 'istanbul',
+    },
+    testTimeout: 1000000,
   },
 });
