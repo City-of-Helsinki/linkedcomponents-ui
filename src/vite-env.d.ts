c140a624f6da36f25aed6072ca18c503816444b2
Jori Lindell <jori.lindell@gmail.com>
2023-10-09 19:26:00 +0300

    Change jest to vitest
    - Configuration for the vitest
    - Remove all jest related packages
    - Add global vitest TypeScript types. Remove **/__mocks__/* from tsconfig.json excludes array to make this to work. This change requires to move some dev dependencies to dependencies
    - Configure coverage reports and junit report output file path
    - Replace jest-axe with vitest-axe

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,10 @@
+/* eslint-disable @typescript-eslint/no-empty-interface */
 /// <reference types="vite/client" />
+import 'vitest';
+
+import type { AxeMatchers } from 'vitest-axe/matchers';
+
+declare module 'vitest' {
+  export interface Assertion extends AxeMatchers {}
+  export interface AsymmetricMatchersContaining extends AxeMatchers {}
+}
