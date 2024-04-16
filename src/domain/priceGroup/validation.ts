0135b91718bc45436b02a7d4cf4762b3e6bfd62c
Jori Lindell <jori.lindell@gmail.com>
2024-04-16 09:58:09 +0300

    fix: validate max lengths of price group fields

--- src/domain/priceGroup/validation.ts
+++ src/domain/priceGroup/validation.ts
@@ -1,16 +1,15 @@
 import * as Yup from 'yup';
 
-import {
-  CHARACTER_LIMITS,
-  LE_DATA_LANGUAGES,
-  ORDERED_LE_DATA_LANGUAGES,
-} from '../../constants';
+import { LE_DATA_LANGUAGES, ORDERED_LE_DATA_LANGUAGES } from '../../constants';
 import {
   createMultiLanguageValidation,
   createStringMaxErrorMessage,
 } from '../../utils/validationUtils';
 import { VALIDATION_MESSAGE_KEYS } from '../app/i18n/constants';
-import { PRICE_GROUP_FIELDS } from './constants';
+import {
+  PRICE_GROUP_FIELDS,
+  PRICE_GROUP_TEXT_FIELD_MAX_LENGTH,
+} from './constants';
 
 export const priceGroupSchema = Yup.object().shape({
   [PRICE_GROUP_FIELDS.PUBLISHER]: Yup.string()
@@ -19,14 +18,17 @@ export const priceGroupSchema = Yup.object().shape({
   [PRICE_GROUP_FIELDS.DESCRIPTION]: createMultiLanguageValidation(
     ORDERED_LE_DATA_LANGUAGES,
     Yup.string().max(
-      CHARACTER_LIMITS.MEDIUM_STRING,
+      PRICE_GROUP_TEXT_FIELD_MAX_LENGTH[PRICE_GROUP_FIELDS.DESCRIPTION],
       createStringMaxErrorMessage
     )
   ).concat(
     Yup.object().shape({
       [LE_DATA_LANGUAGES.FI]: Yup.string()
         .required(VALIDATION_MESSAGE_KEYS.STRING_REQUIRED)
-        .max(CHARACTER_LIMITS.MEDIUM_STRING, createStringMaxErrorMessage),
+        .max(
+          PRICE_GROUP_TEXT_FIELD_MAX_LENGTH[PRICE_GROUP_FIELDS.DESCRIPTION],
+          createStringMaxErrorMessage
+        ),
     })
   ),
 });
