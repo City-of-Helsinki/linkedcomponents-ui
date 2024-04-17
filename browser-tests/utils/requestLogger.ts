90d01a2a82c48f1319e1219224d7fcb75adac4e1
Roope Jansson <roope.jansson@vincit.fi>
2024-04-17 10:03:30 +0300

    test: remove testcafe tests

--- browser-tests/utils/requestLogger.ts
+++
@@ -1,7 +0,0 @@
-import { RequestLogger } from 'testcafe';
-
-import { LINKED_EVENTS_URL } from './settings';
-
-export const requestLogger = RequestLogger(new RegExp(LINKED_EVENTS_URL), {
-  logResponseHeaders: true,
-});
