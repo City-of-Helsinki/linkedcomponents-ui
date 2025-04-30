ecd63ccf666f54d99d3322b908bda658b9f6f4f2
Mikko Jämiä <66477579+mikkojamG@users.noreply.github.com>
2025-04-30 10:33:53 +0300

    revert: cookieBanner enable matomo tracking LINK-1759

--- src/domain/app/hooks/useCookieConsentSettings.ts
+++ src/domain/app/hooks/useCookieConsentSettings.ts
@@ -1,36 +1,15 @@
-import { CookieConsentChangeEvent, CookieConsentReactProps } from 'hds-react';
+import { CookieConsentReactProps } from 'hds-react';
 
 import { PAGE_HEADER_ID } from '../../../constants';
 import useLocale from '../../../hooks/useLocale';
 import siteSettings from './data/siteSettings.json';
-
-export enum COOKIE_CONSENT_GROUP {
-  Tunnistamo = 'tunnistamo',
-  UserInputs = 'userInputs',
-  Shared = 'shared',
-  Statistics = 'statistics',
-}
 
 const useCookieConsentSettings = () => {
   const locale = useLocale();
 
   const cookieConsentProps: CookieConsentReactProps = {
     // eslint-disable-next-line @typescript-eslint/no-empty-function
-    onChange: (changeEvent: CookieConsentChangeEvent) => {
-      const { acceptedGroups } = changeEvent;
-
-      const hasStatisticsConsent =
-        acceptedGroups.indexOf(COOKIE_CONSENT_GROUP.Statistics) > -1;
-
-      if (hasStatisticsConsent) {
-        //  start tracking
-        window._paq.push(['setConsentGiven']);
-        window._paq.push(['setCookieConsentGiven']);
-      } else {
-        // tell matomo to forget conset
-        window._paq.push(['forgetConsentGiven']);
-      }
-    },
+    onChange: () => {},
     siteSettings: siteSettings,
     options: { focusTargetSelector: `#${PAGE_HEADER_ID}`, language: locale },
   };
