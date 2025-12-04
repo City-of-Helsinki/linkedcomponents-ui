eee17b0cc54c537b05cf422ecfefadc565075f23
Timo Juonoja <timo.juonoja@gofore.com>
2025-12-04 08:10:31 +0200

    feat: add new VAT percentage option for 13.5%
    Refs: LINK-2438

--- src/domain/priceGroup/hooks/__tests__/useVatOptions.test.ts
+++ src/domain/priceGroup/hooks/__tests__/useVatOptions.test.ts
@@ -10,6 +10,7 @@ it('should return vat options', async () => {
   expect(result.current).toEqual([
     { label: '25,5 %', value: '25.50' },
     { label: '14 %', value: '14.00' },
+    { label: '13,5 %', value: '13.50' },
     { label: '10 %', value: '10.00' },
     { label: '0 %', value: '0.00' },
   ]);
