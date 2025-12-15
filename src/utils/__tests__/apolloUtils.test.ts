3c70e759e47c860671b3668bded5634f39a18096
mikkojamG <mikko.jamia@gofore.com>
2025-12-15 12:17:16 +0200

    chore: test broken apolloUtils test

--- src/utils/__tests__/apolloUtils.test.ts
+++ src/utils/__tests__/apolloUtils.test.ts
@@ -1,6 +1,6 @@
 import { normalizeKey } from '../apolloUtils';
 
-describe('normalizeKey function', () => {
+describe('normalizeKey function (broken)', () => {
   it('should normalize key', () => {
     expect(normalizeKey('@event_type')).toBe('atEventType');
     expect(normalizeKey('event_end_date')).toBe('eventEndDate');
