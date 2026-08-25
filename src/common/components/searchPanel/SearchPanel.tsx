7f895e3fb1614d84a0ad7479a0b4e161e237c6c4
mikkojamG <mikko.jamia@gofore.com>
2026-08-25 14:19:42 +0300

    fix: searchPanel fallback keys Refs: LINK-2601

--- src/common/components/searchPanel/SearchPanel.tsx
+++ src/common/components/searchPanel/SearchPanel.tsx
@@ -38,8 +38,10 @@ const SearchRow: FC<SearchRowProps> = ({
     <div className={styles.searchRow}>
       <InputWrapper>
         <FilterRow>
-          {mainSelectors?.map((selector) => (
-            <SelectorColumn key={selector.key}>{selector}</SelectorColumn>
+          {mainSelectors?.map((selector, index) => (
+            <SelectorColumn key={selector.key ?? `main-selector-${index}`}>
+              {selector}
+            </SelectorColumn>
           ))}
 
           <TextSearchColumn>
@@ -58,7 +60,10 @@ const SearchRow: FC<SearchRowProps> = ({
         {!!secondarySelectors?.length && (
           <FilterRow>
             {secondarySelectors?.map((selector, index) => (
-              <SelectorColumn key={selector.key} small={index % 3 === 2}>
+              <SelectorColumn
+                key={selector.key ?? `secondary-selector-${index}`}
+                small={index % 3 === 2}
+              >
                 {selector}
               </SelectorColumn>
             ))}
